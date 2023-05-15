cdc5dc43e1efc76bfa9e245c8a78419fca4d2698
Muhammad Abdullah <ma0401431@gmail.com>
2023-05-15 05:58:18 -0700

    Fix: update the position of absolute background images in hero section

--- src/containers/Hero/index.tsx
+++ src/containers/Hero/index.tsx
@@ -33,26 +33,28 @@ const index = () => {
 
   return (
     // HERO SECTION
-    <SectionWrapper className="flex items-center !pt-20 lg:!pt-28 justify-center overflow-hidden">
+    <SectionWrapper className="flex items-center !pt-14 sm:!pt-20 lg:!pt-28 justify-center overflow-hidden">
       {/* MODAL */}
       <Modal isOpen={isOpen} onClose={handleCloseModal}>
         <p>You Entered {number}</p>
       </Modal>
       {/* SVG'S */}
+      {/* BLUE IMAGE FOR SMALL SCREEN DEVICES */}
+      <SvgWrapper src="/HeroSectionPng/SmallScreenBlueImage.png" className="block small-mobile:hidden h-[45vh]  w-full top-[-6%] absolute  -z-10" />
       {/* THE LEFT BLUE ONE */}
       <SvgWrapper
         src="/HeroSectionPng/LeftBlueImage.svg"
-        className="absolute lg:rotate-0 rotate-90 scale-[2.2] sm:scale-[2] lg:scale-100 top-[-25%] sm:top-[-48%] lg:top-[0%] left-[-15%] sm:left-[-23%] md:left-[-16%] lg:left-[-22%] xl:left-[-10%] 2xl:left-[0%] -z-10  sm:h-full w-full h-[397px] sm:max-h-[689px] xl:max-h-[889px] xl:max-w-[1139px]"
+        className="absolute lg:rotate-0 hidden small-mobile:block rotate-90 scale-[2.2] sm:scale-[2] lg:scale-100 top-[-18%] sm:top-[-50%] md:top-[-40%] lg:top-[0%] left-[-15%] sm:left-[-17%] md:left-[-16%] lg:left-[-22%] xl:left-[-10%] 2xl:left-[0%] -z-10 h-[40vh] sm:h-full w-full max-h-[397px] sm:max-h-[689px] xl:max-h-[889px] xl:max-w-[1139px]"
       />
       {/* LIGHT BLUE SVG */}
       <SvgWrapper
         src="/HeroSectionPng/LightBlueImage.svg"
-        className="w-full h-full max-w-[557px] xl:max-w-[657px] max-h-[392px] xl:max-h-[492px] absolute -z-20 left-[-15%] sm:left-[-4%] md:left-[8%] lg:left-[3%] xl:left-[8%] 2xl:left-[15%] top-[15%] sm:top-[15%] md:top-[25%] lg:top-[40%] xl:top-[30%] 2xl:top-[33%]"
+        className="w-full h-full max-w-[557px] xl:max-w-[657px] max-h-[392px] xl:max-h-[492px] absolute -z-20 left-[-15%] small-mobile:left-[-20%] sm:left-[-4%] md:left-[8%] lg:left-[3%] xl:left-[8%] 2xl:left-[15%] top-[25%] small-mobile:top-[23%] sm:top-[15%] md:top-[25%] lg:top-[40%] xl:top-[30%] 2xl:top-[33%]"
       />
       {/* LIGHT BLUE NAVBAR SVG */}
       <SvgWrapper
         src="/HeroSectionPng/LightBlueNavbarImage.svg"
-        className="h-full -z-10 w-full hidden lg:block max-w-[632px] xl:max-w-[732px] max-h-[548px] xl:max-h-[548px] absolute top-[-30%] lg:top-[-30%] xl:top-[-32%]  left-[50%] lg:left-[46%] xl:left-[50%]"
+        className="h-full -z-10 w-full hidden lg:block max-w-[632px] xl:max-w-[732px] max-h-[548px] xl:max-h-[548px] absolute top-[-22%] lg:top-[-34%] xl:top-[-35%] 2xl:top-[-34%] left-[50%] lg:left-[46%] xl:left-[50%]"
       />
 
       {/* RIGHT BLUE SVG */}
@@ -61,7 +63,7 @@ const index = () => {
         className="w-full h-full hidden lg:block max-h-[347px] xl:max-h-[397px] absolute lg:top-[2%] xl:top-[3%] 2xl:top-[5%] lg:right-[-20%] xl:right-[-16%] 2xl:right-[-10%] max-w-[418px] xl:max-w-[468px] -z-20"
       />
 
-      <FlexCenter className="gap-[5rem] sm:gap-[10rem] lg:gap-7 w-full flex-col lg:flex-row !justify-center lg:!justify-around">
+      <FlexCenter className="gap-[10rem] lg:gap-7 w-full flex-col lg:flex-row !justify-center lg:!justify-around">
         {/* LEFT SIDE FORM DIV */}
         <FlexColumn className="p-7 lg:p-5 gap-5  lg:!items-start">
           {/* H2 HEADING */}
