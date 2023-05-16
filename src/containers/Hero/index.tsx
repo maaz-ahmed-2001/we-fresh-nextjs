a710eb795a68ec215e3e0be68066f64b23be8a24
Muhammad Abdullah <ma0401431@gmail.com>
2023-05-16 00:41:04 -0700

    Fix: fixed the positions of background images in the hero section

--- src/containers/Hero/index.tsx
+++ src/containers/Hero/index.tsx
@@ -40,17 +40,17 @@ const index = () => {
       </Modal>
       {/* SVG'S */}
       {/* BLUE IMAGE FOR SMALL SCREEN DEVICES */}
-      <SvgWrapper src="/HeroSectionPng/SmallScreenBlueImage.png" className="block small-mobile:hidden w-[468px] h-[447px] top-[-6%] absolute  -z-10" />
+      <SvgWrapper src="/HeroSectionPng/SmallScreenBlueImage.png" className="block sm:hidden w-[468px] small-mobile:w-full h-[447px] small-mobile:h-[497px] top-[-6%] small-mobile:top-[-10%] absolute  -z-10" />
       {/* THE LEFT BLUE ONE */}
       <SvgWrapper
         src="/HeroSectionPng/LeftBlueImage.svg"
         // imageClassName="object-cover sm:object-fill"
-        className="absolute lg:rotate-0 rotate-90 hidden small-mobile:block scale-[2.2] sm:scale-[2] lg:scale-100 top-[0%] sm:top-[-50%] md:top-[-40%] lg:top-[0%] left-[0%] sm:left-[-17%] md:left-[-16%] lg:left-[-22%] xl:left-[-10%] 2xl:left-[0%] -z-10 h-[40vh] sm:h-full w-full max-h-[397px] sm:max-h-[689px] xl:max-h-[889px] xl:max-w-[1139px]"
+        className="absolute lg:rotate-0 rotate-90 hidden sm:block scale-[2.2] sm:scale-[2] lg:scale-100 sm:top-[-46%] md:top-[-40%] lg:top-[0%] left-[-17%] md:left-[-16%] lg:left-[-22%] xl:left-[-10%] 2xl:left-[0%] -z-10 h-full w-full max-h-[397px] sm:max-h-[689px] xl:max-h-[889px] xl:max-w-[1139px]"
       />
       {/* LIGHT BLUE SVG */}
       <SvgWrapper
         src="/HeroSectionPng/LightBlueImage.svg"
-        className="w-full h-full max-w-[557px] xl:max-w-[657px] max-h-[392px] xl:max-h-[492px] absolute -z-20 left-[-15%] small-mobile:left-[-20%] sm:left-[-4%] md:left-[8%] lg:left-[3%] xl:left-[8%] 2xl:left-[15%] top-[20%] small-mobile:top-[23%] sm:top-[15%] md:top-[25%] lg:top-[40%] xl:top-[30%] 2xl:top-[33%]"
+        className="w-full h-full max-w-[557px] xl:max-w-[657px] max-h-[392px] xl:max-h-[492px] absolute -z-20 left-[-15%] small-mobile:left-[-15%] sm:left-[-6%] md:left-[8%] lg:left-[3%] xl:left-[8%] 2xl:left-[15%] top-[20%] small-mobile:top-[15%] sm:top-[18%] md:top-[25%] lg:top-[40%] xl:top-[30%] 2xl:top-[33%]"
       />
       {/* LIGHT BLUE NAVBAR SVG */}
       <SvgWrapper
@@ -64,7 +64,7 @@ const index = () => {
         className="w-full h-full hidden lg:block max-h-[347px] xl:max-h-[397px] absolute lg:top-[2%] xl:top-[3%] 2xl:top-[5%] lg:right-[-20%] xl:right-[-16%] 2xl:right-[-10%] max-w-[418px] xl:max-w-[468px] -z-20"
       />
 
-      <FlexCenter className="gap-[6rem] sm:gap-[10rem] lg:gap-7 w-full flex-col lg:flex-row !justify-center lg:!justify-around">
+      <FlexCenter className="gap-[5rem] sm:gap-[10rem] lg:gap-7 w-full flex-col lg:flex-row !justify-center lg:!justify-around">
         {/* LEFT SIDE FORM DIV */}
         <FlexColumn className="p-7 lg:p-5 gap-5  lg:!items-start">
           {/* H2 HEADING */}
